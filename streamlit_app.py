5c528fa33cf1d9749fab21b6fadd01177e8d142a
Julien Barbas <118379917+julienbadge2@users.noreply.github.com>
2022-11-16 17:52:09 +0100

    Update streamlit_app.py
    added text

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,12 @@ import streamlit
 
 
 streamlit.title('My ParentsNew Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+
+streamlit.text('Kale,Spinash & Rocket Smoothie')
+
+
+streamlit.text('Hard-Boiled free-range Egg')
