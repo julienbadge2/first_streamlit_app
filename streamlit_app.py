2d869c5f4a4471b8a9eeab86644c35eb9f4b07ee
Julien Barbas <118379917+julienbadge2@users.noreply.github.com>
2022-11-23 14:27:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,4 +13,5 @@ streamlit.text('🥗 Kale,Spinash & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled free-range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-streamlit.dataframe(my_fruit_list)
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(fruits_to_show)
